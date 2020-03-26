ff0130198ec68a1bfb954fd364d760d47e7e4cfa
Florian Polier <florian.polier@heig-vd.ch>
2020-03-26 19:09:22 +0100

    probes are almost self registering into DB

--- Client-API/MAC.py
+++ Client-API/MAC.py
@@ -6,21 +6,28 @@ class MAC:
         self.isRandom = isRandom
         self.fk_vendor = fk_vendor
     
-    def mac_to_int(self):
-        res = re.match('^((?:(?:[0-9a-f]{2}):){5}[0-9a-f]{2})$', self.address.lower())
+    @staticmethod
+    def mac_to_int(address):
+        res = re.match('^((?:(?:[0-9a-f]{2}):){5}[0-9a-f]{2})$', address.lower())
         if res is None:
             raise ValueError('invalid mac address')
         return int(res.group(0).replace(':', ''), 16)
 
-    def int_to_mac(self, intAddress):
+    @staticmethod
+    def int_to_mac(intAddress):
         if type(intAddress) != int:
             raise ValueError('invalid integer')
         return ':'.join(['{}{}'.format(a, b)
                         for a, b
                         in zip(*[iter('{:012x}'.format(intAddress))]*2)])
 
-    def isLocallyAssigned(self):
-        intAddress = self.mac_to_int()
+    @staticmethod
+    def isLocallyAssigned(address):
+        intAddress = MAC.mac_to_int(address)
         # set the 41th bit to one and the others to zero
         mask = 0x20000000000
         return bool(((intAddress & mask) >> 41) | 0)
+
+    @staticmethod
+    def extractVendor(address):
+        return address[:8]
--- Client-API/scanProb.py
+++ Client-API/scanProb.py
@@ -7,6 +7,7 @@ import datetime
 from MAC import MAC
 from Place import Place
 from Probe import Probe
+from APIException import *
 
 
 parser = argparse.ArgumentParser(description="Scan for probes request")
@@ -21,12 +22,21 @@ def postMAC(mac):
     api_url = '{0}macs'.format(api_url_base)
     mac_json = {'address':mac.address, 'isRandom':mac.isRandom, 'fk_vendor':mac.fk_vendor}
     response = requests.post(api_url, headers=headers, json=mac_json)
-    print(response)
+    if response.status_code != 201:
+        raise APIError(response.status_code)
+
+    return response.text
 
 def getMACByAddress(address):
     api_url = '{0}macs/{1}'.format(api_url_base, address)
     response = requests.get(api_url, headers=headers)
-    print(response)
+    if response.status_code != 200:
+        if response.status_code != 404:
+            raise APIError(response.status_code)
+        else:
+            raise APIErrorNotFound(response.status_code)
+
+    return response.text
 
 def postProbeRequest(probe):
     api_url = '{0}probes'.format(api_url_base)
@@ -40,7 +50,18 @@ def analyzePacket(packet):
         #print(packet.show())
         # Le paquet est-il un paquet de management et est une probe request
         if packet.type == 0 and packet.subtype == 4:
-            probe = Probe(packet.info.decode('utf-8'), 1, 1)
+            try:
+                address = packet.info.decode('utf-8')
+                mac = getMACByAddress(address)
+            except APIErrorNotFound:
+                try:
+                    mac = MAC(address, MAC.isLocallyAssigned(address), MAC.extractVendor(address))
+                    mac = postMAC(mac)
+                except:
+                    return
+            except APIError:
+                return
+            probe = Probe(mac, 1, 1)
             postProbeRequest(probe)
             print("SSID: ", packet.addr2)
             print("ESSID : ", packet.info.decode('utf-8'))
@@ -52,10 +73,9 @@ def analyzePacket(packet):
 #sniff(iface=args.i, prn=analyzePacket)
 
 probe = Probe("Coucou", 1, "FF:FF:AB:FF:FF:FF")
-mac = MAC("FF:FF:AB:FF:FF:FF", True, "F8-4D-33")
+mac = MAC("FF:FF:AB:FF:FF:FJ", True, "F8-4D-33")
 mac2 = MAC("FF:FF:AB:FF:FF:FA", True, "F8-4D-33")
-
-print(mac.isLocallyAssigned())
+print(postMAC(mac))
 
 
 
