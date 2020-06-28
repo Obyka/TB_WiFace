7d6ee7388cc0e22eadf81c8ad98a74d6a9bc7463
Florian Polier <polier.florian@gmail.com>
2020-06-28 18:00:31 +0200

    Picture upload moved and specified in OpenAPI spec

--- API/server.py
+++ API/server.py
@@ -45,34 +45,6 @@ def expired_token_callback(callback):
 	resp.headers.setlist('Set-Cookie', response.headers.getlist('Set-Cookie'))
 	return resp, 302
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-@connex_app.route('/api/upload', methods=['POST'])
-def upload_file():
-	# check if the post request has the file part
-	if 'file' not in request.files:
-		resp = jsonify({'message' : 'No file part in the request'})
-		resp.status_code = 400
-		return resp
-	file = request.files['file']
-	if file.filename == '':
-		resp = jsonify({'message' : 'No file selected for uploading'})
-		resp.status_code = 400
-		return resp
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(config.app.config['UPLOAD_FOLDER'], filename))
-		handle_picture(file, filename)
-		resp = jsonify({'message' : 'File successfully uploaded'})
-		resp.status_code = 201
-		return resp
-	else:
-		resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
-		resp.status_code = 400
-		return resp
-
-
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
 	connex_app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=('cert.pem', 'key.pem'))
---
+++ API/upload.py
@@ -0,0 +1,33 @@
+import config
+import os
+from flask import request, jsonify, redirect
+from werkzeug.utils import secure_filename
+from recognize_face import handle_picture
+from dateutil.relativedelta import relativedelta
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def upload():
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		resp = jsonify({'message' : 'No file part in the request'})
+		resp.status_code = 400
+		return resp
+	file = request.files['file']
+	if file.filename == '':
+		resp = jsonify({'message' : 'No file selected for uploading'})
+		resp.status_code = 400
+		return resp
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(config.app.config['UPLOAD_FOLDER'], filename))
+		handle_picture(file, filename)
+		resp = jsonify({'message' : 'File successfully uploaded'})
+		resp.status_code = 201
+		return resp
+	else:
+		resp = jsonify({'message' : 'Allowed file types are ' + str(ALLOWED_EXTENSIONS)})
+		resp.status_code = 400
+		return resp
