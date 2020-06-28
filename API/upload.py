a00431a26f1d29082f028fd6cba42b346b83325d
Florian Polier <polier.florian@gmail.com>
2020-06-28 18:59:53 +0200

    Improving file upload to be a bit more secure

--- API/upload.py
+++ API/upload.py
@@ -4,6 +4,7 @@ from flask import request, jsonify, redirect
 from werkzeug.utils import secure_filename
 from recognize_face import handle_picture
 from dateutil.relativedelta import relativedelta
+import time
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 def allowed_file(filename):
@@ -21,9 +22,16 @@ def upload():
 		resp.status_code = 400
 		return resp
 	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
+		filename = secure_filename('face_upload_%s.%s'%(time.strftime("%Y%m%d-%H%M%S"), file.filename.split('.')[-1]))
+        #save data to file system
 		file.save(os.path.join(config.app.config['UPLOAD_FOLDER'], filename))
-		handle_picture(file, filename)
+		try:
+			handle_picture(file, filename)
+		except:
+			resp = jsonify({'message' : 'An error occured while handling the picture'})
+			resp.status_code = 500
+			return resp
+
 		resp = jsonify({'message' : 'File successfully uploaded'})
 		resp.status_code = 201
 		return resp
