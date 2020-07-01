1a4decb7559ac218c2deb40bce602b55f6edbb33
Obyka <polier.florian@gmail.com>
2020-07-01 13:47:35 +0200

    Flipping image to improve detection rate with profile cascade

--- Client-API/recognize_face.py
+++ Client-API/recognize_face.py
@@ -11,6 +11,35 @@ from Auth import User
 from Identity import Identity, Represents
 from Picture import Picture
 
+def process_frame(frame, MyAPI):
+    """Given a frame, this function will call all the cascades on it and flip it
+    for the profile one. Then it sends the result to the API. 
+
+    Args:
+        frame: picture to process
+        MyAPI (API): API object to use to send the results
+    """
+    base_cascade_path = cv.data.haarcascades
+    face_cascade_list = [
+        os.path.join(base_cascade_path, 'haarcascade_frontalface_alt2.xml'),
+        os.path.join(base_cascade_path, 'haarcascade_frontalface_alt_tree.xml'),
+        os.path.join(base_cascade_path, 'haarcascade_frontalface_alt.xml'),
+        os.path.join(base_cascade_path, 'haarcascade_frontalface_default.xml'),
+        os.path.join(base_cascade_path, 'haarcascade_profileface.xml')
+    ]
+
+    image_list = []
+
+    for x in face_cascade_list:
+        image_list_faces, frame = detectFace(frame, x)
+        image_list.extend(image_list_faces)
+
+        if "haarcascade_profileface" in x:
+            image_list_faces, frame = detectFace(frame, x)
+            image_list.extend(image_list_faces)
+            
+    for im in image_list:
+        MyAPI.postFile(im)
 
 def TestWithoutOpenCV(image_path, MyAPI):
     """This function allows to send an image without OpenCV processing
@@ -22,12 +51,12 @@ def TestWithoutOpenCV(image_path, MyAPI):
     MyAPI.postFile(image_path)
 
 
-def detectFace(frame):
+def detectFace(frame, cascade):
     """OpenCV face detection using pre-trained cascade
 
     Arguments:
         frame (file) -- [image which may contain a face]
-        face_cascade (file) -- [pre-trained cascade to detect face]
+        cascade (string) -- [path to pre-trained cascade to detect face]
 
     Returns:
         (face_detected) -- [Does the image contain a face?]
@@ -36,45 +65,33 @@ def detectFace(frame):
 
     timestr = time.strftime("%Y%m%d-%H%M%S")
     image_list = []
-    i = 0
 
-    base_cascade_path = cv.data.haarcascades
-    face_cascade_list = [
-        os.path.join(base_cascade_path, 'haarcascade_frontalface_alt2.xml'),
-        os.path.join(base_cascade_path, 'haarcascade_frontalface_alt_tree.xml'),
-        os.path.join(base_cascade_path, 'haarcascade_frontalface_alt.xml'),
-        os.path.join(base_cascade_path, 'haarcascade_frontalface_default.xml'),
-        os.path.join(base_cascade_path, 'haarcascade_profileface.xml')
-    ]
+    face_cascade = cv.CascadeClassifier()
 
-    for x in face_cascade_list:
-        face_cascade = cv.CascadeClassifier()
-        # Load the cascades
-        if not face_cascade.load(cv.samples.findFile(x)):
-            print('--(!)Error loading face cascade ' + str(x))
-            continue
+    # Load the cascades
+    if not face_cascade.load(cv.samples.findFile(cascade)):
+        print('--(!)Error loading face cascade ' + str(cascade))
+        return image_list
 
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(frame,
-                                              scaleFactor=1.1,
-                                              minNeighbors=5,
-                                              minSize=(30, 30),
-                                              flags=cv.CASCADE_SCALE_IMAGE)
-        print("Found {0} faces!".format(len(faces)) + " - " + str(x))
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,
+                                            scaleFactor=1.1,
+                                            minNeighbors=5,
+                                            minSize=(30, 30),
+                                            flags=cv.CASCADE_SCALE_IMAGE)
+    print("Found {0} faces!".format(len(faces)) + " - " + str(cascade))
 
-        for (x, y, w, h) in faces:
-            roi_color = frame[y:y + h, x:x + w]
-            print("[INFO] Object found. Saving locally.")
-            image = '{0}/image_{1}_{2}.png'.format(directory, timestr, i)
-            cv.imwrite(image, roi_color)
-            cv.rectangle(frame, (int(x - 0.1*w), int(y - 0.1*h)),
-                         (int(x + w + 0.1*w), int(y + h + 0.1*h)), (0, 255, 0), -1)
-            image_list.append(image)
-            i += 1
-    if i > 0:
+    for i, (x, y, w, h) in enumerate(faces):
+        roi_color = frame[y:y + h, x:x + w]
+        image = '{0}/image_{1}_{2}.png'.format(directory, timestr, i)
+        cv.imwrite(image, roi_color)
+        cv.rectangle(frame, (int(x - 0.1*w), int(y - 0.1*h)),
+                        (int(x + w + 0.1*w), int(y + h + 0.1*h)), (0, 255, 0), -1)
+        image_list.append(image)
+    if len(faces) > 0:
         image = '{0}/complete_image_{1}.png'.format(directory, timestr)
         cv.imwrite(image, frame)
-    return image_list
+    return image_list, frame
 
 
 def main():
@@ -92,17 +109,15 @@ def main():
     creds = User(username, password)
     MyAPI = API(creds, args.api)
 
+
     if args.debug and args.frame is not None:
         TestWithoutOpenCV(args.frame, MyAPI)
         return
 
     if args.frame is not None:
         frame = cv.imread(args.frame)
-        image_list = detectFace(frame)
-        print(len(image_list))
-        for im in image_list:
-            print(im)
-            MyAPI.postFile(im)
+        process_frame(frame, MyAPI)
+        
     else:
         camera_device = args.camera
         # Read the video stream
@@ -125,10 +140,8 @@ def main():
                 print('--(!) No captured frame -- Break!')
                 break
 
-            image_list = detectFace(frame)
+            process_frame(frame, MyAPI)
 
-            for image in image_list:
-                MyAPI.postFile(image)
 
         # When everything done, release the capture
         cam.release()
