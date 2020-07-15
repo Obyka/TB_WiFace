bad9f2668968d5aa5d8178b8813b8726ab8b241b
Obyka <polier.florian@gmail.com>
2020-07-15 16:31:57 +0200

    refactor to be more RESTFUL in URLs

--- API/web/web.py
+++ API/web/web.py
@@ -57,12 +57,15 @@ def inject_counters():
 def inject_path():
 	return dict(picture_path=config.app.config['UPLOAD_FOLDER'])
 
+@web_bp.route('/places/<place_id>', methods=['GET'])
+@admin_required
+def place_front(place_id):
+	place_data = places.get_place_infos(places.read_one(place_id))
+	return render_template('place_details.html', place=place_data)
+
 @web_bp.route('/places')
 @admin_required
 def places_front():
-	if 'id' in request.args:
-		place_data = places.get_place_infos(places.read_one(request.args.get('id')))
-		return render_template('place_details.html', place=place_data)
 	places_list = places.read_all()
 	return render_template('places.html', places_list=places_list)
 
@@ -149,61 +152,58 @@ def statistics_front():
 	feed = [((verbose_timedelta(datetime.datetime.utcnow() - i[0])), i[1]) for i in pictures.feed()]
 	return render_template('statistics.html', feed=feed)
 
-@web_bp.route('/represents')
+@web_bp.route('/represents/<represent_id>', methods=['GET'])
 @admin_required
-def represents_front():
-	if 'id' in request.args:
-		identity = identities.read_one(request.args.get('id'))
+def represents_front(represent_id):
+		identity = identities.read_one(represent_id)
 		represents_datas = identities.read_pictures(identity.get('id'))
 		pictures_list = []
 		for represents_data in represents_datas:
 			pictures_list.append(pictures.read_one(represents_data.get('fk_picture')))
 		return render_template('represents.html', pictures_list=pictures_list, identity=identity)
-	else:
-		r = redirect('/web/identities')
-		return r
 
-@web_bp.route('/identities/<identity_id>', methods=['POST'])
+@web_bp.route('/identities/<identity_id>', methods=['GET', 'POST', 'DELETE'])
 def identity_form_front(identity_id):
-	form = IdentityForm(request.form)
-	schema = IdentitiesSchema()
-	if form.csrf_token.validate(IdentityForm):
-		old_identity = identities.read_one(identity_id)
-		old_identity = schema.load(old_identity)
-		old_identity.mail=form.email.data if form.email.validate(IdentityForm) else old_identity.mail
-		old_identity.firstname=form.firstname.data if form.firstname.validate(IdentityForm) else old_identity.firstname
-		old_identity.lastname=form.lastname.data if form.lastname.validate(IdentityForm) else old_identity.lastname
-		serialized_identity=schema.dump(old_identity)
-		identities.edit_identity(identity_id, serialized_identity)
-		return jsonify({'firstname': old_identity.firstname, 'lastname':old_identity.lastname, 'mail':old_identity.mail}), 201
-	else:
-		return '', 500
-@web_bp.route('/identities', methods=['GET', 'POST', 'DELETE'])
+	if request.method == "GET":
+		identity_form = IdentityForm()
+		identity = identities.read_one(identity_id)
+		nb_picture = pictures.count_by_id(identity.get('id'))
+		mac_addresses = belongsto.read_by_identity(identity.get('id'))
+		age_range = identities.read_age_range(identity.get('id'))
+		gender_result = identities.read_gender(identity.get('id'))
+		best_pic = pictures.read_best_pic(identity.get('id'))
+		pictures_place = pictures.get_picture_place_by_identity(identity.get('id'))
+		avatar_path = avatar.draw_avatar(best_pic)
+		best_macs = mariage.best_fit(mac_addresses) if mac_addresses else []
+		mac_datas = []
+		for mac in best_macs:
+			mac_datas.append(macs.get_mac_infos(macs.read_one(mac.get('fk_mac'))))
+		return render_template('identity_details.html',identity_form=identity_form, pictures_place=pictures_place, nb_picture=nb_picture, age_range=age_range,identity=identity, best_pic=best_pic, best_macs=best_macs, gender=gender_result, mac_datas=mac_datas,avatar_path=avatar_path)
+
+	if request.method == "DELETE":
+		try:
+			identities.delete(identity_id)
+		except HTTPException:
+			return '', 404
+		return '', 204
+
+	if request.method == "POST":
+		form = IdentityForm(request.form)
+		schema = IdentitiesSchema()
+		if form.csrf_token.validate(IdentityForm):
+			old_identity = identities.read_one(identity_id)
+			old_identity = schema.load(old_identity)
+			old_identity.mail=form.email.data if form.email.validate(IdentityForm) else old_identity.mail
+			old_identity.firstname=form.firstname.data if form.firstname.validate(IdentityForm) else old_identity.firstname
+			old_identity.lastname=form.lastname.data if form.lastname.validate(IdentityForm) else old_identity.lastname
+			serialized_identity=schema.dump(old_identity)
+			identities.edit_identity(identity_id, serialized_identity)
+			return jsonify({'firstname': old_identity.firstname, 'lastname':old_identity.lastname, 'mail':old_identity.mail}), 201
+		else:
+			return '', 500
+@web_bp.route('/identities', methods=['GET', 'POST'])
 @admin_required
 def identities_front():
-	if request.method == "DELETE":
-		if 'id' in request.args:
-			try:
-				identities.delete(request.args.get('id'))
-			except HTTPException:
-				return '', 404
-			return '', 204
-	if 'id' in request.args:
-		if request.method == "GET":
-			identity_form = IdentityForm()
-			identity = identities.read_one(request.args.get('id'))
-			nb_picture = pictures.count_by_id(identity.get('id'))
-			mac_addresses = belongsto.read_by_identity(identity.get('id'))
-			age_range = identities.read_age_range(identity.get('id'))
-			gender_result = identities.read_gender(identity.get('id'))
-			best_pic = pictures.read_best_pic(identity.get('id'))
-			pictures_place = pictures.get_picture_place_by_identity(identity.get('id'))
-			avatar_path = avatar.draw_avatar(best_pic)
-			best_macs = mariage.best_fit(mac_addresses) if mac_addresses else []
-			mac_datas = []
-			for mac in best_macs:
-				mac_datas.append(macs.get_mac_infos(macs.read_one(mac.get('fk_mac'))))
-		return render_template('identity_details.html',identity_form=identity_form, pictures_place=pictures_place, nb_picture=nb_picture, age_range=age_range,identity=identity, best_pic=best_pic, best_macs=best_macs, gender=gender_result, mac_datas=mac_datas,avatar_path=avatar_path)
 	identitiy_list = identities.read_all()
 	if request.method == 'POST':
 		for identity in identitiy_list:
@@ -213,22 +213,22 @@ def identities_front():
 		return redirect('/web/identities')
 	return render_template('identities.html', identitiy_list=identitiy_list)
 
+@web_bp.route('/macs/<mac_id>', methods=['GET', 'DELETE'])
+@admin_required
+def mac_front(mac_id):
+	if request.method == "DELETE":
+		try:
+			macs.delete(mac_id)
+		except HTTPException:
+			return '', 404
+		return '', 204
+	if request.method == "GET":
+		mac_data = macs.get_mac_infos(macs.read_one(mac_id))
+		return render_template('mac_details.html', mac_data=mac_data)
 @web_bp.route('/macs', methods=['GET', 'POST', 'DELETE'])
 @admin_required
 def macs_front():
-	if request.method == "DELETE":
-		if 'id' in request.args:
-			try:
-				macs.delete(request.args.get('id'))
-			except HTTPException:
-				return '', 404
-			return '', 204
-
 	mac_list = macs.read_all()
-	if 'id' in request.args:
-		mac_data = macs.get_mac_infos(macs.read_one(request.args.get('id')))
-		return render_template('mac_details.html', mac_data=mac_data)
-	
 	if request.method == 'POST':
 		for mac in mac_list:
 			new_PP2I = mac['address'] in request.form
@@ -240,14 +240,11 @@ def macs_front():
 			mac =  macs.get_mac_infos(mac)
 		return render_template('macs.html', mac_list=mac_list)
 
-@web_bp.route('/pictures')
+@web_bp.route('/pictures/<picture_id>', methods=['GET'])
 @admin_required
-def pictures_front():
-	if 'id' not in request.args:
-		abort(404)
-	id = request.args.get('id')
+def pictures_front(picture_id):
 	emotions = ['happy', 'surprised', 'fear', 'confused', 'sad', 'calm', 'disgusted', 'angry']
-	current_pic = pictures.read_one(id)
+	current_pic = pictures.read_one(picture_id)
 	avatar_path = avatar.draw_avatar(current_pic)
 	# retire les trois expressions principales de la photo et les trie
 	main_emotions = sorted([(emotion, current_pic.get(emotion)) for count, emotion in enumerate(emotions) if current_pic.get(emotion) and abs(current_pic.get(emotion)) >= 10], key=lambda emotion: emotion[1], reverse=True)[:3]
